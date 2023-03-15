0ae8cf33ffddecd81ce9695a39b05af6b96b8a7e
Filippo Bonafini <filippo.bonafini@outlook.it>
2023-03-15 20:32:32 +0100

    Refactoring commenti

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 'use strict';
 
 // INIZIALIZZIAMO VUE 
-const { createApp } = Vue
+const { createApp } = Vue;
 
 // STRUTTURA DI VUE 
 createApp({
   data() {
     return {
+      // DATO CHE SI AUTOSCRIVE IN BASE ALL'INPUT DELL'UTENTE 
       elementName:'',
+        // OGGETTI NELLA LISTA 
         toDoList:[
           {
             elementName:'fare la spesa',
@@ -25,19 +27,28 @@ createApp({
     }
   },
   methods:{
+    
+    // FUNZIONE CHE ELIMINA UNA VOCE DALL'ARRAY DEGLI OGGETTI 
     deleteItem(index){
       this.toDoList.splice(index,1);
     },
+
+    // FUNZIONE CHE IMPOSTA UNA VOCE 'FATTA'
     doneItem(index){
       this.toDoList[index].done = true;
     },
+
+    // FUNZIONE CHE CREA UN NUOVO ELEMENTO 
     createItem(){
       const newElement = {
         elementName: this.elementName,
         done: false
       }
-      this.toDoList.push(newElement)
+      this.toDoList.push(newElement);
+      this.elementName = '';
     },
+
+    // FUNZIONE CHE RENDE NON FATTO UN ELEMENTO 
     saveItem(index){
       this.toDoList[index].done = false;
     }
